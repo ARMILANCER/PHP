d9c12066dbb1b70679e96e2fa820cc06a9be7f21
ARMILANCER <114140097+ARMILANCER@users.noreply.github.com>
2023-11-29 11:42:21 +0100

    Delete Test.php

--- Test.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Effettua un redirect a un'altra pagina dopo 5 secondi
-header("Refresh: 5; URL=https://www.example.com");
-echo "Stai per essere reindirizzato a un'altra pagina in 5 secondi...";
-?>

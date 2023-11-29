733512973a23766b0fa69fa5136f7330d990f187
ARMILANCER <114140097+ARMILANCER@users.noreply.github.com>
2023-11-29 12:10:40 +0100

    Update Regiter.php

--- Regiter.php
+++ Regiter.php
@@ -1,27 +1,24 @@
 <?php
 
-function register(){
-    // Connection
-    $con = new mysqli("localhost","root","root","utenti");
-    if($con->connect_error){
-        die("Connessione fallita ".$con->connect_error);
-    }
-    // Get values of HTLM page
-    $nome = $_POST["nome"];
-    $cognome = $_POST["cognome"];
-    $email = $_POST["email"];
-    $password = $_POST["password"];
-    $yob = $_POST["yob"];
-    $hobby = $_POST["hobby"];
+// Connection
+$con = new mysqli("localhost","root","root","utenti");
+if($con->connect_error){
+    die("Connessione fallita ".$con->connect_error);
+}
+// Get values of HTLM page
+$nome = $_POST["nome"];
+$cognome = $_POST["cognome"];
+$email = $_POST["email"];
+$password = $_POST["password"];
+$yob = $_POST["yob"];
+$hobby = $_POST["hobby"];
 
-    $sql = "INSERT INTO dati(Nome,Cognome,Email,Password,YOB,Hobby)
-    VALUES('$nome','$cognome','$email','$password','$yob','$hobby');";
-    if($con->query($sql)){echo"FINE";}else{echo"nou";};
-    echo memory_get_usage()."\n";
-
+$sql = "INSERT INTO dati(Nome,Cognome,Email,Password,YOB,Hobby)
+VALUES('$nome','$cognome','$email','$password','$yob','$hobby');";
+if($con->query($sql)){
     header("Refresh: 3; URL=https://www.example.com");
     echo "\nStai per essere reindirizzato a un'altra pagina in 3 secondi...";
-}
-
+}else{echo"nou";};
+echo memory_get_usage()."\n";
 ?>
 
